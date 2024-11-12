01002af3b789623380c35dae90d4851a969ac29a
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2024-11-12 16:23:00 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,6 +4,7 @@ mvn clean package
 ONEAGENT_ENVIRONMENT_HOST=$(echo $ONEAGENT_INSTALLER_SCRIPT_URL | sed -E 's|https?://([^/]+).*|\1|')
 ENVIRONMENT_ID=$(echo "$url" | sed -E 's#https://([^.]+).*#\1#')
 
+echo "docker login https://$ONEAGENT_ENVIRONMENT_HOST --username $ENVIRONMENT_ID --password $ONEAGENT_INSTALLER_DOWNLOAD_TOKEN"
 docker login https://$ONEAGENT_ENVIRONMENT_HOST --username $ENVIRONMENT_ID --password $ONEAGENT_INSTALLER_DOWNLOAD_TOKEN
 docker build -t codespaces-playground
 docker run -d codespaces-playground
