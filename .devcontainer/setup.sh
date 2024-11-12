df200a3ea8fcf6898ba74aed4ae68c9d8c97a2ec
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2024-11-12 15:31:28 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,9 +2,9 @@
 mvn clean package
 
 ONEAGENT_ENVIRONMENT_HOST=$(echo $ONEAGENT_INSTALLER_SCRIPT_URL | sed -E 's|https?://([^/]+).*|\1|')
+ENVIRONMENT_ID=$(echo "$url" | sed -E 's#https://([^.]+).*#\1#')
 
-jq --arg registry "$ONEAGENT_ENVIRONMENT_HOST" --arg token "$ONEAGENT_INSTALLER_DOWNLOAD_TOKEN" '.auths[$registry] = { "auth": $token }' "/home/vscode/.docker/config.json" > "/home/vscode/.docker/config.json.tmp" && mv "/home/vscode/.docker/config.json.tmp" "/home/vscode/.docker/config.json"
-
+docker login https://$ONEAGENT_ENVIRONMENT_HOST --username $ENVIRONMENT_ID --password $ONEAGENT_INSTALLER_DOWNLOAD_TOKEN
 docker build -t codespaces-playground
 docker run -d codespaces-playground
 
