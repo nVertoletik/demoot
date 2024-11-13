d4ebd4b2af3d46d6bcd0040d8252b923e6f9d7ef
Reinhard-Pilz-Dynatrace <reinhard.pilz@dynatrace.com>
2024-11-13 14:30:36 +0000

    updates

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "setup"
 # mvn clean package
 
-# ONEAGENT_ENVIRONMENT_HOST=$(echo $ONEAGENT_INSTALLER_SCRIPT_URL | sed -E 's|https?://([^/]+).*|\1|')
-# ENVIRONMENT_ID=$(echo "$ONEAGENT_INSTALLER_SCRIPT_URL" | sed -E 's#https://([^.]+).*#\1#')
-# docker login https://$ONEAGENT_ENVIRONMENT_HOST --username $ENVIRONMENT_ID --password $ONEAGENT_INSTALLER_DOWNLOAD_TOKEN
-# docker build -t codespaces-playground .
-# docker run -d codespaces-playground
+wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
+ONEAGENT_ENVIRONMENT_HOST=$(echo $ONEAGENT_INSTALLER_SCRIPT_URL | sed -E 's|https?://([^/]+).*|\1|')
+echo "export ONEAGENT_ENVIRONMENT_HOST=$ONEAGENT_ENVIRONMENT_HOST" >> ~/.bashrc
+ENVIRONMENT_ID=$(echo "$ONEAGENT_INSTALLER_SCRIPT_URL" | sed -E 's#https://([^.]+).*#\1#')
+docker login https://$ONEAGENT_ENVIRONMENT_HOST --username $ENVIRONMENT_ID --password $ONEAGENT_INSTALLER_DOWNLOAD_TOKEN
---
+++ loadgen/src/main/java/com/dtcookie/shop/load/LoadGenerator.java
@@ -0,0 +1,35 @@
+package com.dtcookie.shop.load;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.dtcookie.shop.Ports;
+import com.dtcookie.util.Streams;
+
+
+public class LoadGenerator extends TimerTask {
+
+	private static final Timer timer = new Timer(false);
+	private static final ExecutorService executor = Executors.newCachedThreadPool();
+
+	public static void main(String[] args) throws Exception {		
+		timer.schedule(new LoadGenerator(), 0, 1000);
+	}
+
+	@Override
+	public void run() {
+		executor.submit(LoadGenerator::sendRequest);
+	}
+
+	public static Object sendRequest() throws Exception {
+		URL url = new URL("http://localhost:" + Ports.FRONTEND_LISTEN_PORT + "/place-order");
+		try (InputStream in = url.openStream()) {
+			Streams.drain(in);
+		}
+		return null;
+	}
+}
