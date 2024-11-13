3216c708bc71b0aa0d9ecac08fd8b2fdb37b142d
Reinhard-Pilz-Dynatrace <reinhard.pilz@dynatrace.com>
2024-11-13 14:35:00 +0000

    modified:   .devcontainer/setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,6 +4,6 @@
 
 wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
 ONEAGENT_ENVIRONMENT_HOST=$(echo $ONEAGENT_INSTALLER_SCRIPT_URL | sed -E 's|https?://([^/]+).*|\1|')
-echo "export ONEAGENT_ENVIRONMENT_HOST=$ONEAGENT_ENVIRONMENT_HOST" >> ~/.bashrc
+echo "ONEAGENT_ENVIRONMENT_HOST=$ONEAGENT_ENVIRONMENT_HOS" | sudo tee -a /etc/environment
 ENVIRONMENT_ID=$(echo "$ONEAGENT_INSTALLER_SCRIPT_URL" | sed -E 's#https://([^.]+).*#\1#')
 docker login https://$ONEAGENT_ENVIRONMENT_HOST --username $ENVIRONMENT_ID --password $ONEAGENT_INSTALLER_DOWNLOAD_TOKEN
