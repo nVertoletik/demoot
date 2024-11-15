721431feaf04404ccea9bf0e51303da42b8f990f
Reinhard-Pilz-Dynatrace <reinhard.pilz@dynatrace.com>
2024-11-15 09:37:55 +0000

    fri-02

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,19 +2,34 @@
 
 # mvn clean package
 
-ENVIRONMENT_ID=$(echo "$DT_ENVIRONMENT_URL" | sed -E 's#https://([^.]+).*#\1#')
+ENVIRONMENT_ID=$(echo "$DT_ENVIRONMENT_URL" | sed -E 's#^https?://([a-zA-Z0-9\-]+)\..*\..*#\1#;s#^([a-zA-Z0-9\-]+)\..*\..*#\1#;s#^([a-zA-Z0-9\-]+)#\1#')
+if echo "$DT_ENVIRONMENT_URL" | grep -q ".dynatracelabs.com"; then
+    DT_DOMAIN="dynatracelabs.com"
+else
+    DT_DOMAIN="dynatrace.com"
+fi
+
+# Set DT_STAGE based on whether the URL contains ".sprint."
+if echo "$DT_ENVIRONMENT_URL" | grep -q ".sprint."; then
+    DT_STAGE="sprint"
+else
+    DT_STAGE="live"
+fi
+
+DT_ENVIRONMENT_HOST="$ENVIRONMENT_ID.$DT_STAGE.$DT_DOMAIN"
+
+
 sudo sh -c "echo \"ENVIRONMENT_ID=$ENVIRONMENT_ID\" >> /etc/environment"
 echo "export ENVIRONMENT_ID=$ENVIRONMENT_ID" >> ~/.bashrc
 export ENVIRONMENT_ID=$ENVIRONMENT_ID
 
-DT_ENVIRONMENT_HOST="$ENVIRONMENT_ID.live.dynatrace.com"
-export DT_ENVIRONMENT_HOST
 sudo sh -c "echo \"DT_ENVIRONMENT_HOST=$DT_ENVIRONMENT_HOST\" >> /etc/environment"
 echo "export DT_ENVIRONMENT_HOST=$DT_ENVIRONMENT_HOST" >> ~/.bashrc
+export DT_ENVIRONMENT_HOST
 
 sed -i "s/DYNATRACE_DOCKER_REGISTRY/$DT_ENVIRONMENT_HOST/g" ./frontend/Dockerfile
 
-sed -i "s/#alias ll='ls -l'/alias ll='ls -l'/g" ~/.bashrc
+sed -i "s/#alias ll='ls -l'/alias ll='ls -al'/g" ~/.bashrc
 
 # Download OpenTelemetry Java Auto Instrumentation Agent
 wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM maven:3.8.5-openjdk-17-slim
-WORKDIR /app
-
-COPY common/prefetch/pom.xml ./
-RUN mvn dependency:go-offline -Dmaven.repo.local=app/.m2 -B
